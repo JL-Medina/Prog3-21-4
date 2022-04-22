cbbd2cdb52f4b9e87ed1efe3d0d3e6076c4d3283
JL <josemedina1610@gmail.com>
2022-04-21 21:51:21 -0300

    Agregar archivos de proyecto.

---
+++ Persona.cs
@@ -0,0 +1,29 @@
+﻿/*Crear un programa en C# que solicite al usuario tres nombres de personas y los almacene 
+en un lista de objetos de tipo Persona. Para ello, crear primero una clase Persona 
+que tenga una propiedad Nombre de tipo string. 
+Además, la clase persona debe sobreescribir el método ToString() produciendo el siguiente resultado: "Hola! mi nombre es {Nombre}.".
+Finalizar el programa ejecutando el método ToString() para cada persona en la pantalla.*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog3_21_4
+{
+    public class Persona
+    {
+        private string _Name;
+        public Persona(string name)
+        {
+            _Name = name;
+
+        }
+
+        public override string ToString()
+        {
+            return $"Hola! Mi nombre es {_Name}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace Prog3_21_4
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            List<Persona> personas = new();
+
+            for(var i = 0; i < 3; i++) 
+            { 
+                Console.WriteLine($"Ingrese el nombre {i+1}: ");
+                personas.Add(new Persona(Console.ReadLine()));
+            }
+            foreach(var persona in personas)
+            {
+                Console.WriteLine(persona.ToString());
+            }
+            
+        }
+    }
+}
